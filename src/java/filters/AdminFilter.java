df156a3db114a53cb6e3a4fce4d78bb6ed90594a
jifang <jifang1218@gmail.com>
2023-03-20 12:34:40 -0600

    init.

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/AdvancedFilter.java to edit this template
+ */
+package filters;
+
+import dataaccess.UserDB;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Role;
+import models.User;
+import services.AccountService;
+
+/**
+ *
+ * @author jifang
+ */
+public class AdminFilter implements Filter {
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+            // code that is executed before the servlet
+            HttpServletRequest httpRequest = (HttpServletRequest)request;
+            HttpSession session = httpRequest.getSession();
+            String email = (String)session.getAttribute("email");
+            UserDB userDB = new UserDB();
+            User user = userDB.get(email);
+            Role role = user.getRole();
+            HttpServletResponse httpResponse = (HttpServletResponse)response;
+            if (role.getRoleId() != 1 ) { // sysadmin, redirect to admin
+                httpResponse.sendRedirect("notes");
+                return;
+            } 
+            
+            // continue to servlet
+            chain.doFilter(request, response); // execute the servlet
+            
+            // code that is executed after the servlet
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    @Override
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    @Override
+    public void init(FilterConfig filterConfig) {        
+    }
+
+}
